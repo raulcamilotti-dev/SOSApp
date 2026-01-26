04b01db64bd92b2c7d5d37c8cb5660aaf46f3329
raulcamilotti-dev <raulcamilotti@gmail.com>
2026-01-26 19:18:57 -0300

    coomiit

--- core/context/ThemeContext.tsx
+++ core/context/ThemeContext.tsx
@@ -3,9 +3,11 @@ import { useAppConfig } from "./AppConfigContext";
 
 const { branding } = useAppConfig();
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const theme = {
   colors: {
-    primary: branding.primaryColor,
-    background: branding.backgroundColor,
+    primary: branding.colors.primary,
+    background: branding.colors.background,
+    header: branding.colors.header, 
   }
 };
